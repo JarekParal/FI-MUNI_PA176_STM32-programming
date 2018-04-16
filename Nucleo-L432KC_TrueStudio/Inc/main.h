9590f984e3a36d499ee1c5f267273cf01901bd39
Jarek Páral <paral.jarek@gmail.com>
2018-04-16 23:56:32 +0200

    ExNucleo: GPIO - Read button -> Write to LED

--- Nucleo-L432KC_TrueStudio/Inc/main.h
+++ Nucleo-L432KC_TrueStudio/Inc/main.h
@@ -53,6 +53,8 @@
 #define MCO_GPIO_Port GPIOA
 #define VCP_TX_Pin GPIO_PIN_2
 #define VCP_TX_GPIO_Port GPIOA
+#define PA12_SW1_Pin GPIO_PIN_12
+#define PA12_SW1_GPIO_Port GPIOA
 #define SWDIO_Pin GPIO_PIN_13
 #define SWDIO_GPIO_Port GPIOA
 #define SWCLK_Pin GPIO_PIN_14
@@ -61,6 +63,8 @@
 #define VCP_RX_GPIO_Port GPIOA
 #define LD3_Pin GPIO_PIN_3
 #define LD3_GPIO_Port GPIOB
+#define PB4_L_R_Pin GPIO_PIN_4
+#define PB4_L_R_GPIO_Port GPIOB
 
 /* ########################## Assert Selection ############################## */
 /**
--- Nucleo-L432KC_TrueStudio/Src/main.c
+++ Nucleo-L432KC_TrueStudio/Src/main.c
@@ -110,9 +110,12 @@ int main(void)
 
 	  // Simple LED blinking - alternative
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 0);
-	  HAL_Delay(100);
+	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 1);
-	  HAL_Delay(100);
+	  HAL_Delay(500);
+
+	  // GPIO - Read button SW1 -> Write to LED L_R
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12));
 
   /* USER CODE END WHILE */
 
@@ -241,14 +244,20 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LD3_Pin|PB4_L_R_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LD3_Pin */
-  GPIO_InitStruct.Pin = LD3_Pin;
+  /*Configure GPIO pin : PA12_SW1_Pin */
+  GPIO_InitStruct.Pin = PA12_SW1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(PA12_SW1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD3_Pin PB4_L_R_Pin */
+  GPIO_InitStruct.Pin = LD3_Pin|PB4_L_R_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD3_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
